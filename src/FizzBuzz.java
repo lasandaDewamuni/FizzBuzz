fdc5fb6a37c998ce30400d231ae00f8c4febbbed
lasandaDewamuni <132021752+lasandaDewamuni@users.noreply.github.com>
2023-09-11 18:47:54 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,9 +2,15 @@
  * Solve the FizzBuzz challenge.
  */
 class FizzBuzz {
-
+    /* Notes about Java syntax:
+     *  - all conditions are in parentheses
+     *  - all lines of code end with ;
+     *  - name of data type is written when declaring a variable
+     *  - instead of elif it is else if
+     *  - instead of 'and' double ampersand (&&) is used for the and operator
+     */
     public static void main(String[] args) {
-
+        /* FOR LOOP IMPLEMENTATION:
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
@@ -29,6 +35,30 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+        } */
+        // TASK 1.1: REWRITE THIS USING WHILE
+        int i = 1;
+        while (i < 100){
+
+            i = doFizzBuzz(i);
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy5 && divisibleBy3) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+
+        i++;
+        return i;
     }
 }
