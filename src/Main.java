21d60b537b04309ea19036b7beea059441ec69d5
lasandaDewamuni <132021752+lasandaDewamuni@users.noreply.github.com>
2023-09-11 20:14:15 -0400

    Task 3.2 completion update

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        String[] test = {"Array of strings"};
+        FizzBuzz.main(test);
+    }
+}
